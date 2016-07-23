1571fe549a35d03c55f3ff18e776acc7b44a3a04
Михаил Синяков <pvpshoot@gmail.com>
2016-07-23 11:03:15 +0400

    add redux to project

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+	render() {
+		return (
+			<div className="ecwid-g-r">
+		    <div class="ecwid-u-1"><p>filter</p></div>
+		    <div class="ecwid-u-1"><p>content</p></div>
+			</div>
+
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+EcwidApp.init({
+  app_id: "bulk-stock-control-dev", // place your application namespace here (not clientID)
+  autoloadedflag: true,
+  autoheight: true,
+});
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
